708da0b371ff91c143ad82f134c9cdc70d2d38d6
Ben-Van-Raemdonck <Ben-vanraemdonck@hotmail.com>
2022-03-11 11:26:54 +0100

    change app to update a revised bom list

--- bom_update.py
+++ bom_update.py
@@ -1,41 +1,52 @@
-import openpyxl
 import pandas as pd
-from my_openpyxl import find_column, write_column
+import numpy as np
 
 
-def main(bom_single_path: str, bom_hier_path: str):
+def main(bom_old_path: str, bom_new_path: str):
     # read df from excel
-    bom_single = pd.read_excel(bom_single_path, header=1)
-    bom_hier = pd.read_excel(bom_hier_path, header=1)
+    bom_old = pd.read_excel(bom_old_path, header=1)
+    bom_new = pd.read_excel(bom_new_path, header=1)
     # columns to update
-    update_columns = ['In stock', 'Besteld', 'Status', 'Besteldatum', 'Leveringsdatum', 'Palletnr.']
+    update_columns = ['In stock', 'Besteld', 'Besteldatum', 'Leveringsdatum', 'Serienr.', 'Tagnr.', 'Palletnr.', 'Status']
     # columns to check
-    columns = ['Component', 'Component description', 'PartNumber', 'Size', 'Length', 'Diameter', 'Thickness']
+    check_columns = ['Component', 'Component description', 'PartNumber', 'Size', 'Length', 'Diameter', 'Thickness']
 
-    bom_hier_update = bom_hier.loc[:, update_columns].copy()
-
-    for i in range(len(bom_hier)):
+    list_update_rows = []
+    revision_series = pd.Series([np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, 'Revisie!'],
+                                index=update_columns)
+    for i in range(len(bom_new)):
         # value is the same or both are na
-        mask1 = (bom_single.loc[:, columns] == bom_hier.loc[i, columns]) | \
-                (pd.isna(bom_single.loc[:, columns]) & pd.isna(bom_hier.loc[i, columns]))
+        mask1 = (bom_old.loc[:, check_columns] == bom_new.loc[i, check_columns]) | \
+                (pd.isna(bom_old.loc[:, check_columns]) & pd.isna(bom_new.loc[i, check_columns]))
         # reduce to single column, with True if all values in the row were True
         mask2 = mask1.all(axis=1)
-        # the row of the single component bom list that's the same as the row of the hierarchical bom list
-        same_row = bom_single[mask2]
-        # sets the status
-        try:
-            bom_hier_update.loc[i, :] = same_row.loc[same_row.index[0], :]
-        except IndexError:  # if the row wasn't found, write '-' in all columns
-            bom_hier_update.loc[i, :] = '-'
+        # the row of the old bom list that's the same as the row of the new bom list
+        same_row = bom_old[mask2]
 
-    bom_hier.loc[:, update_columns] = bom_hier_update
+        # if the row wasn't found add 'Revisie!' as Status
+        if not mask2.any():
+            row_series = revision_series.append(bom_new.loc[i, :])
+            list_update_rows.append(row_series)
+        else:
+            qty_old = same_row.loc[same_row.index[0], 'Quantity']
+            qty_new = bom_new.loc[i, 'Quantity']
+            row_series = same_row.loc[same_row.index[0], update_columns].append(bom_new.loc[i, :])
+            # if the quantity is the same in the old and new bom list
+            if qty_old == qty_new:
+                list_update_rows.append(row_series)
+            # if the quantity is different
+            else:
+                # add a row with the old quantity and status
+                row_series.loc['Quantity'] = qty_old
+                list_update_rows.append(row_series)
+                # add a row with the rest of the quantity and 'Revisie!' as status
+                row_series = revision_series.append(bom_new.loc[i, :])
+                row_series.loc['Quantity'] = qty_new - qty_old
+                list_update_rows.append(row_series)
 
-    # open the excel file with openpyxl
-    wb = openpyxl.load_workbook(bom_hier_path)
-    ws = wb.active
-    # write columns
-    for col in update_columns:
-        col_excel = find_column(ws, col)
-        write_column(ws, col_excel, bom_hier.loc[:, col])
+    df_update = pd.DataFrame(list_update_rows, index=range(len(list_update_rows)))
+    df_update.to_excel('result.xlsx', index=False, startrow=1)
 
-    wb.save(bom_hier_path)
+
+if __name__ == '__main__':
+    main('test bom 1.xlsx', 'test bom 2.xlsx')
--- main.py
+++ main.py
@@ -6,26 +6,26 @@ import bom_update
 class MainApp(MDApp):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.bom_single_path = ''
-        self.bom_hier_path = ''
+        self.bom_old_path = ''
+        self.bom_new_path = ''
 
     def build(self):
         self.theme_cls.theme_style = 'Dark'
         self.theme_cls.primary_palette = 'Blue'
         pass
 
-    def set_bom_single_path(self):
-        self.bom_single_path = filechooser.open_file(title='choose single component BOM list',
-                                                     filters=[('Excel file', '*.xlsx')])[0]
-        self.root.ids.lab_single_path.text = self.bom_single_path
+    def set_bom_old_path(self):
+        self.bom_old_path = filechooser.open_file(title='choose single component BOM list',
+                                                  filters=[('Excel file', '*.xlsx')])[0]
+        self.root.ids.lab_old_path.text = self.bom_old_path
 
-    def set_bom_hier_path(self):
-        self.bom_hier_path = filechooser.open_file(title='choose hierarchical BOM list',
-                                                   filters=[('Excel file', '*.xlsx')])[0]
-        self.root.ids.lab_hier_path.text = self.bom_hier_path
+    def set_bom_new_path(self):
+        self.bom_new_path = filechooser.open_file(title='choose hierarchical BOM list',
+                                                  filters=[('Excel file', '*.xlsx')])[0]
+        self.root.ids.lab_new_path.text = self.bom_new_path
 
     def update_bom(self):
-        bom_update.main(self.bom_single_path, self.bom_hier_path)
+        bom_update.main(self.bom_old_path, self.bom_new_path)
         self.root.ids.run_button.text = 'BOM list updated!'
 
 
